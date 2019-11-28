93e8795cc9c178489852c278cc17568c3ecb1e84
Martin Prot <martinprot@gmail.com>
2019-11-28 11:17:37 +0100

    missing some public accessors

--- Coordinator/NavigationCoordinator.swift
+++ Coordinator/NavigationCoordinator.swift
@@ -237,7 +237,7 @@ private extension NavigationCoordinator {
 
 extension NavigationCoordinator {
 	
-	func push(_ viewController: UIViewController, animated: Bool = true, completion: @escaping () -> Void = {}) {
+	public func push(_ viewController: UIViewController, animated: Bool = true, completion: @escaping () -> Void = {}) {
 		viewControllers.append(viewController)
 		self.rootViewController.pushViewController(viewController, animated: animated)
 		guard animated, let coordinator = self.rootViewController.transitionCoordinator else {
@@ -247,7 +247,7 @@ extension NavigationCoordinator {
 		coordinator.animate(alongsideTransition: nil) { _ in completion() }
 	}
 	
-	func root(_ viewController: UIViewController, animated: Bool, completion: (() -> Void)? = nil) {
+	public func root(_ viewController: UIViewController, animated: Bool, completion: (() -> Void)? = nil) {
 		if animated {
 			self.push(viewController) {
 				self.root(viewController)
